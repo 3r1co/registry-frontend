87bbf4a3c048ca3303312648a48910427d74a4cd
Eric Muellenbach <emuellenbach@gmx.de>
2019-01-30 14:36:02 +0100

    Add matrix job to Travis to build and test both Node and Python (include code coverage)
    f

--- api/manifest.py
+++ api/manifest.py
@@ -1,8 +1,10 @@
 import asyncio
+
 import aiohttp
-from helpers.constants import MANIFEST_PREFIX
-from sanic import Blueprint, response
 from rejson import Path
+from sanic import Blueprint, response
+
+from helpers.constants import MANIFEST_PREFIX
 
 manifest = Blueprint('manifest', url_prefix='/manifest')
 
@@ -11,9 +13,10 @@ async def tag_handler(request, repo, tag):
     loop = asyncio.get_event_loop()
     key = repo + "/" + tag
     app = request.app
-    manifest = app.db.jsontype(MANIFEST_PREFIX + key, Path.rootPath()) if app.persistent else app.manifests[key]
-    if manifest is not None:
-        return response.json(app.db.jsonget(MANIFEST_PREFIX + key, Path.rootPath())) if app.persistent else response.json(manifest)
+    mf = app.db.jsontype(MANIFEST_PREFIX + key, Path.rootPath()) if app.persistent else app.manifests.get(key, None)
+    if mf is not None:
+        resp = app.db.jsonget(MANIFEST_PREFIX + key, Path.rootPath()) if app.persistent else mf
+        return response.json(resp)
     else:
         async with aiohttp.ClientSession(loop=loop) as session:
             resp = await app.reg.retrieve_manifest_v1_for_tag_and_repository(repo, tag, session)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,13 +28,13 @@ class App extends Component {
             <div>
                 <NavBar />
                 <Grid container spacing={24} style={{padding: 24, margin: 0, width: '100%'}}>
-                    <Grid item xs="4">
+                    <Grid item xs={4}>
                         <RepositoryTable onClick={this.clickRepository} />
                     </Grid>
-                    <Grid item xs="4">
+                    <Grid item xs={4}>
                         <TagsTable ref={this.tagTable} onClick={this.clickTag} />
                     </Grid>
-                    <Grid item xs="4">
+                    <Grid item xs={4}>
                         <ManifestCard innerRef={this.manifestCard} />
                     </Grid>
                 </Grid>
